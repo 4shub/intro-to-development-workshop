c88ef6ce54257e5e303baef2e0534030486d8aab
Shubham Naik <shubham.naik10@gmail.com>
2017-06-02 17:10:14 -0700

    Initial Commit

---
+++ src/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to HackMerced!</h2>
+        </div>
+        <p className="App-intro">
+          This is a demo of what we can build at HackMerced :)
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
